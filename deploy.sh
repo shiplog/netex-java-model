78e8f213adbb89ba43c09cf5c337bb6561d80a38
Jakob Vad Nielsen <jakobvadnielsen@gmail.com>
2019-08-19 19:50:01 +0200

    Fixed deploy

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build -t netexjavamodel .
+docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -v $HOME/.m2:/root/.m2 -w /usr/src/mymaven netexjavamodel mvn clean deploy
