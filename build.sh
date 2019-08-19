615cf48b8de808b9a1c5186735d314cec6034378
Jakob Vad Nielsen <jakobvadnielsen@gmail.com>
2019-08-19 13:29:15 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t netexjavamodel .
-docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven netexjavamodel mvn clean install
+docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -v $HOME/.m2:/root/.m2 -w /usr/src/mymaven netexjavamodel mvn clean install
